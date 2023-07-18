aea5ae06aba384cecc926e563e7cfaf7f133e9a9
saulpadilla10 <saulp1021@yahoo.com>
2023-07-17 18:56:15 -0700

    weatherIs

---
+++ WeatherAPI/Map.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherAPI
+{
+	public class Map
+	{
+		public static double GetTemp(string weatherToday)
+		{
+			var client = new HttpClient();
+
+			var response = client.GetStringAsync(weatherToday).Result;
+
+			var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            var name = JObject.Parse(response)["name"].ToString();
+
+			Console.WriteLine(name);
+			return temp;
+		} 
+	}
+}
+
---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+
+namespace WeatherAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Enter zipcode:");
+            string zip = Console.ReadLine();
+
+            var weatherToday = $"http://api.openweathermap.org/data/2.5/weather?zip={zip}&units=imperial&appid={APIKey}";
+
+
+            Console.WriteLine($"Current temp: {Map.GetTemp(weatherToday)}F");
+
+
+
+        }
+
+    }
+
+
+
+
+
+}
+
+
+
